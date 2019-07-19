041027b9c92b2f67ea18aeb1dce6679014a74454
frozenpine <sonny.frozenpine@gmail.com>
2019-07-19 21:05:27 +0800

    匹配多种时间格式

--- model_time.go
+++ model_time.go
@@ -1,20 +1,18 @@
 package ngerest
 
 import (
-	// "os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
+var timestampPattern = regexp.MustCompile("[0-9]+")
+
 // NGETime NGE timestamp competibal with UTC time string & timestamp
 type NGETime struct {
 	time.Time
 }
-
-// func (t *NGETime) String() string {
-// 	return t.Format("2006-01-02 15:04:05.000Z")
-// }
 
 // UnmarshalJSON convert time string or timestamp(ms)
 func (t *NGETime) UnmarshalJSON(data []byte) error {
@@ -24,42 +22,26 @@ func (t *NGETime) UnmarshalJSON(data []byte) error {
 	}
 
 	dataStr := string(data)
+	dataStr = strings.Trim(dataStr, "\" ")
+
 	var err error
 
-	if strings.HasPrefix(dataStr, "\"") && strings.HasSuffix(dataStr, "\"") {
-		dataStr = strings.Trim(dataStr, "\" ")
+	if timestampPattern.MatchString(dataStr) {
+		timestamp, err := strconv.Atoi(dataStr)
 
-		if dataStr == "" {
-			t.Time = time.Unix(0, 0)
-			return nil
+		if err != nil {
+			return err
 		}
 
-		t.Time, err = time.ParseInLocation(
-			"2006-01-02 15:04:05.000Z", dataStr, time.UTC)
+		sec := int64(timestamp / 1000)
+		nsec := (int64(timestamp) - sec*1000) * 1000
+		t.Time = time.Unix(sec, nsec)
 
-		return err
+		return nil
 	}
 
-	timestamp, err := strconv.Atoi(dataStr)
-	sec := int64(timestamp / 1000)
-	nsec := (int64(timestamp) - sec*1000) * 1000
-	t.Time = time.Unix(sec, nsec)
+	t.Time, err = time.ParseInLocation(
+		"2006-01-02 15:04:05.000Z", dataStr, time.UTC)
 
-	return nil
+	return err
 }
-
-// MarshalJSON convert Time structure in json
-// func (t *NGETime) MarshalJSON() ([]byte, error) {
-// 	useTimestamp := os.Getenv("USE_TIMESTAMP")
-
-// 	useTimestamp = strings.Trim(useTimestamp, " \"")
-
-// 	switch useTimestamp {
-// 	case "1", "y", "Y", "t", "T", "yes", "Yes", "YES", "true", "True", "TRUE":
-// 		ms := int(t.UnixNano() / 1000)
-// 		data := []byte(strconv.Itoa(ms))
-// 		return data, nil
-// 	default:
-// 		return []byte("\"" + t.String() + "\""), nil
-// 	}
-// }
